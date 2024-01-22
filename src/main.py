94a6481f4ef254d2eedb15147f03b3a5b803fa41
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-01-22 22:39:38 +0100

    Refactor extract variable

--- src/main.py
+++ src/main.py
@@ -41,15 +41,18 @@ class StringColumnAnalyzer:
 
     def max_values_if_stripped(self) -> tp.List[str]:
         condition = self._df[f"{self._column_name_stripped}_length"] == self.max_length_if_stripped()
-        return get_df_values_of_column(self._column_name_stripped, condition, self._df)
+        column_name = self._column_name_stripped
+        return get_df_values_of_column(column_name, condition, self._df)
 
     def max_values_if_no_stripped(self) -> tp.List[str]:
         condition = self._df[f"{self._column_name}_length"] == self.max_length_if_no_stripped()
-        return get_df_values_of_column(self._column_name, condition, self._df)
+        column_name = self._column_name
+        return get_df_values_of_column(column_name, condition, self._df)
 
     def min_values_if_stripped(self) -> tp.List[str]:
         condition = self._df[f"{self._column_name_stripped}_length"] == self.min_length_if_stripped()
-        return get_df_values_of_column(self._column_name_stripped, condition, self._df)
+        column_name = self._column_name_stripped
+        return get_df_values_of_column(column_name, condition, self._df)
 
     def min_values_if_no_stripped(self) -> tp.List[str]:
         return self._df.loc[self._df[f"{self._column_name}_length"] == self.min_length_if_no_stripped()][
