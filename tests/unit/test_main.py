3c13c1965ad9f48ea6f7112032d1e0c616eca49d
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-02-18 20:06:36 +0100

    Refactor move code

--- tests/unit/test_main.py
+++ tests/unit/test_main.py
@@ -43,17 +43,17 @@ class TestStringColumnAnalyzer(unittest.TestCase):
         result = analysis.has_null_values()
         self.assertTrue(result)
 
-    def test_has_empty_values_if_stripped_is_true(self):
-        column = pd.Series(data=[" "], name="values")
-        analysis = main.StringColumnAnalyzer(column)
-        result = analysis.has_empty_values_if_stripped()
-        self.assertTrue(result)
-
     def test_has_empty_values_if_stripped_is_false(self):
         column = pd.Series(data=[" a"], name="values")
         analysis = main.StringColumnAnalyzer(column)
         result = analysis.has_empty_values_if_stripped()
         self.assertFalse(result)
+
+    def test_has_empty_values_if_stripped_is_true(self):
+        column = pd.Series(data=[" "], name="values")
+        analysis = main.StringColumnAnalyzer(column)
+        result = analysis.has_empty_values_if_stripped()
+        self.assertTrue(result)
 
     def test_has_empty_values_if_no_stripped_is_false(self):
         column = pd.Series(data=[" "], name="values")
