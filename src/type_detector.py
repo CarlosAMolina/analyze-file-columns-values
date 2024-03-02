3813534b0e8cdaca878deb592d47ed4d45a4eea1
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-03-02 17:42:49 +0100

    Refactor extract function and rename

--- src/type_detector.py
+++ src/type_detector.py
@@ -1,30 +1,38 @@
+import typing as tp
+
 from pandas import Series
 import pandas as pd
 
 
 class IntegerTypeDetector:
-    def __init__(self, column: Series):
-        self._column = column
+    def __init__(self, column_strings: Series):
+        self._column_strings = column_strings
 
     def is_column_of_this_type(self) -> bool:
-        try:
-            column_numeric = pd.to_numeric(self._column, errors="raise")
-            if pd.api.types.is_integer_dtype(column_numeric) is True:
-                return True
-        except ValueError:
+        column_numeric = _get_column_as_numeric(self._column_strings)
+        if column_numeric is None:
             return False
+        else:
+            return pd.api.types.is_integer_dtype(column_numeric)
 
 
 class DecimalTypeDetector:
-    def __init__(self, column: Series):
-        self._column = column
+    def __init__(self, column_strings: Series):
+        self._column_strings = column_strings
 
     def is_column_of_this_type(self) -> bool:
-        try:
-            column_numeric = pd.to_numeric(self._column, errors="raise")
-            return column_numeric.dtype == "float64"
-        except ValueError:
+        column_numeric = _get_column_as_numeric(self._column_strings)
+        if column_numeric is None:
             return False
+        else:
+            return column_numeric.dtype == "float64"
+
+
+def _get_column_as_numeric(column: Series) -> tp.Optional[Series]:
+    try:
+        return pd.to_numeric(column, errors="raise")
+    except ValueError:
+        return None
 
 
 # TODO rm
