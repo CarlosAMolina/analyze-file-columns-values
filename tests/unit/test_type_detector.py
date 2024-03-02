8829a23a9a3912e038f9e7507581a1f3796cae28
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-03-02 19:35:50 +0100

    Add test

--- tests/unit/test_type_detector.py
+++ tests/unit/test_type_detector.py
@@ -36,9 +36,9 @@ class TestIntegerTypeAnalyzer(unittest.TestCase):
         column = pd.Series(data=[" 1e1", "2 "], name="values")
         self.assertTrue(ta._IntegerTypeAnalyzer(column).is_column_of_this_type())
 
-    # TODO def test_is_column_of_this_type_is_false_if_e_number_is_decimal(self):
-    # TODO     column = pd.Series(data=[" 1.11e1", "2 "], name="values")
-    # TODO     self.assertFalse(ta._IntegerTypeAnalyzer(column).is_column_of_this_type())
+    def test_is_column_of_this_type_is_false_if_e_number_is_decimal(self):
+        column = pd.Series(data=[" 1.001e1", "2 "], name="values")
+        self.assertFalse(ta._IntegerTypeAnalyzer(column).is_column_of_this_type())
 
     # TODO def test_is_column_of_this_type_is_false_if_negative_e(self):
     # TODO     column = pd.Series(data=[" 1e-1", "2 "], name="values")
