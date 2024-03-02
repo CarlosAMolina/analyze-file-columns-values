520871714f8b1970411851b946e00795ba4b6704
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-03-02 20:22:55 +0100

    Change variable to compare. Refactor drop old code and rename

--- src/type_analyzer.py
+++ src/type_analyzer.py
@@ -20,13 +20,15 @@ class _IntegerTypeAnalyzer:
         self._column_strings = column_strings
 
     def is_column_of_this_type(self) -> bool:
-        if _is_numeric_column(self._column_strings) is False:
+        column_numeric = _get_column_as_numeric(self._column_strings)
+        if column_numeric is None:
             return False
+        # TODO maybe int numbers has thousand separator
         elif self._has_any_decimal_separator():
             return False
         elif _has_column_any_character(self._column_strings):
             if _are_all_characters_e(self._column_strings):
-                return _has_numeric_column_with_e_numbers_only_int_numbers(self._column_strings)
+                return _has_numeric_column_of_e_numbers_only_int_numbers(column_numeric)
             else:
                 return False
         return True
@@ -45,7 +47,7 @@ class _DecimalTypeAnalyzer:
             return False
         elif _has_column_any_character(self._column_strings):
             if _are_all_characters_e(self._column_strings):
-                return not _has_numeric_column_with_e_numbers_only_int_numbers(self._column_strings)
+                return not _has_numeric_column_of_e_numbers_only_int_numbers(column_numeric)
             else:
                 return False
         else:
@@ -65,19 +67,13 @@ def _are_all_characters_e(column: "pd.Series[str]") -> bool:
     return not rows_with_characters.str.contains(r"[a-d]|[f-z]", flags=re.IGNORECASE, regex=True).all()
 
 
-def _has_numeric_column_with_e_numbers_only_int_numbers(column: "pd.Series[str]") -> bool:
-    column_numeric = _get_column_as_numeric(column)
-    column_numeric_str = column_numeric.astype(str)
-    decimal_values: pd.Series[str] = column_numeric_str.str.split(pat=".", expand=True)[1]
+def _has_numeric_column_of_e_numbers_only_int_numbers(column: "pd.Series[int|float]") -> bool:
+    column_str = column.astype(str)
+    decimal_values: pd.Series[str] = column_str.str.split(pat=".", expand=True)[1]
     # Convert to numeric an integer adds `.0`.
     return (decimal_values == "0").all()
 
 
-def _is_numeric_column(column: pd.Series) -> bool:
-    return _get_column_as_numeric(column) is not None
-
-
-# TODO rm and work only with _is_numeric_column?
 def _get_column_as_numeric(column: pd.Series) -> tp.Optional[pd.Series]:
     # TODO. Conflict with the logic when analyzing the column values because
     # withe spaces are not tripped when reading the csv file
