c3943e91e393d9b677aa960dddb2b7629f9208b6
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-03-02 18:26:26 +0100

    Change import

--- src/type_detector.py
+++ src/type_detector.py
@@ -1,15 +1,14 @@
 import re
 import typing as tp
 
-from pandas import Series
 import pandas as pd
 
 
-def is_integer(column_strings: Series):
+def is_integer(column_strings: pd.Series):
     return _IntegerTypeAnalyzer(column_strings).is_column_of_this_type()
 
 
-def is_decimal(column_strings: Series):
+def is_decimal(column_strings: pd.Series):
     return _DecimalTypeAnalyzer(column_strings).is_column_of_this_type()
 
 
@@ -17,7 +16,7 @@ REGEX_DECIMAL_SEPARATOR = r"\."
 
 
 class _IntegerTypeAnalyzer:
-    def __init__(self, column_strings: Series):
+    def __init__(self, column_strings: pd.Series):
         self._column_strings = column_strings
 
     def is_column_of_this_type(self) -> bool:
@@ -37,7 +36,7 @@ class _IntegerTypeAnalyzer:
 
 
 class _DecimalTypeAnalyzer:
-    def __init__(self, column_strings: Series):
+    def __init__(self, column_strings: pd.Series):
         self._column_strings = column_strings
 
     def is_column_of_this_type(self) -> bool:
@@ -48,7 +47,7 @@ class _DecimalTypeAnalyzer:
             return column_numeric.dtype == "float64"
 
 
-def _is_numeric_column(column: Series) -> bool:
+def _is_numeric_column(column: pd.Series) -> bool:
     # TODO. Conflict with the logic when analyzing the column values because
     # withe spaces are not tripped when reading the csv file
     column_stripped = column.str.strip()
@@ -56,7 +55,7 @@ def _is_numeric_column(column: Series) -> bool:
 
 
 # TODO rm and work only with _is_numeric_column?
-def _get_column_as_numeric(column: Series) -> tp.Optional[Series]:
+def _get_column_as_numeric(column: pd.Series) -> tp.Optional[pd.Series]:
     try:
         return pd.to_numeric(column, errors="raise")
     except ValueError:
