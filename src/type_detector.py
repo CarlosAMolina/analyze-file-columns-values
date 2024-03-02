26a918f0df10fb0eef922c93465ebfe4f32df6ef
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-03-02 18:11:04 +0100

    Refactor extract function

--- src/type_detector.py
+++ src/type_detector.py
@@ -21,8 +21,7 @@ class _IntegerTypeDetector:
         self._column_strings = column_strings
 
     def is_column_of_this_type(self) -> bool:
-        column_numeric = _get_column_as_numeric(self._column_strings)
-        if column_numeric is None:
+        if _is_numeric_column(self._column_strings) is False:
             return False
         elif self._has_any_character():
             return False
@@ -49,6 +48,11 @@ class _DecimalTypeDetector:
             return column_numeric.dtype == "float64"
 
 
+def _is_numeric_column(column: Series) -> bool:
+    return _get_column_as_numeric(column) is not None
+
+
+# TODO rm and work only with _is_numeric_column?
 def _get_column_as_numeric(column: Series) -> tp.Optional[Series]:
     try:
         return pd.to_numeric(column, errors="raise")
