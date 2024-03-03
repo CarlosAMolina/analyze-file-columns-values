ea6e627fac2fc3b2c5884943704f0f79f97d2dbf
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-03-03 20:06:13 +0100

    Refactor inline method

--- src/manager.py
+++ src/manager.py
@@ -5,7 +5,4 @@ from src import type_analyzer
 
 
 def get_column_names_and_types_from_df(file_df: pd.DataFrame) -> tp.Dict[str, str]:
-    result = {}
-    for column_name, column_rows in file_df.items():
-        result[column_name] = type_analyzer.get_column_type(column_rows)
-    return result
+    return {column_name: type_analyzer.get_column_type(column_rows) for column_name, column_rows in file_df.items()}
