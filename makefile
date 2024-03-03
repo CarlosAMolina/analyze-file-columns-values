64f90ba3bfa703dbfed6ccd3d05229cbb0db23a8
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-03-03 16:06:00 +0100

    Add manager

--- makefile
+++ makefile
@@ -1,5 +1,5 @@
 test:
 	python -m unittest discover -s tests
 test-filter:
-	python -m unittest discover -s tests -k test_is_column_of_this_type_is_false_if_e_number_is_integer
+	python -m unittest discover -s tests -k test_expected_result_is_returned
 
---
+++ tests/unit/test_manager.py
@@ -0,0 +1,30 @@
+import pathlib
+import unittest
+
+from pandas import DataFrame as Df
+import pandas as pd
+import numpy as np
+
+from src import extractors
+from src import manager
+from src import type_analyzer
+
+
+class TestFunction_get_column_names_and_types_from_df(unittest.TestCase):
+    def test_expected_result_is_returned(self):
+        df = get_df_from_csv_test_file("file.csv")
+        result = manager.get_column_names_and_types_from_df(df)
+        expected_result = {
+            "Column string": type_analyzer.Type.STRING,
+            "Column string all lines with value": type_analyzer.Type.STRING,
+            "Column integer": type_analyzer.Type.INTEGER,
+            "Column ecimal": type_analyzer.Type.DECIMAL,
+        }
+        self.assertEqual(result, expected_result)
+
+
+def get_df_from_csv_test_file(file_name: str) -> Df:
+    script_dir = pathlib.Path(__file__).parent.absolute()
+    tests_dir = script_dir.parent
+    csv_file_path_name = str(pathlib.PurePath(tests_dir, "files", file_name))
+    return extractors.get_df_from_csv(csv_file_path_name)
