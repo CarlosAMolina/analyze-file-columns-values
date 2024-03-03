eb69c66cfb9d8819ea566ad20e8f8e7f62a96ea1
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-03-03 20:22:45 +0100

    Change function add iterator

--- src/manager.py
+++ src/manager.py
@@ -5,11 +5,12 @@ from src import extractors
 from src import type_analyzer
 
 
-def show_file_analisis(file_path_name: str) -> tp.Dict[str, str]:
+def show_file_analisis(file_path_name: str):
     file_df = extractors.get_df_from_csv(file_path_name)
-    for column_name, column_type in _get_column_names_and_types_from_df(file_df).items():
+    for column_name, column_type in _get_column_names_and_types_from_df(file_df):
         print(column_name, column_type)  # TODO rm
 
 
-def _get_column_names_and_types_from_df(file_df: pd.DataFrame) -> tp.Dict[str, str]:
-    return {column_name: type_analyzer.get_column_type(column_rows) for column_name, column_rows in file_df.items()}
+def _get_column_names_and_types_from_df(file_df: pd.DataFrame) -> tp.Iterator[tp.Tuple[str, str]]:
+    for column_name, column_rows in file_df.items():
+        yield column_name, type_analyzer.get_column_type(column_rows)
--- tests/unit/test_manager.py
+++ tests/unit/test_manager.py
@@ -17,13 +17,13 @@ class TestFunction_show_file_analysis(unittest.TestCase):
 class TestFunction_get_column_names_and_types_from_df(unittest.TestCase):
     def test_expected_result_is_returned(self):
         df = get_df_from_csv_test_file("file.csv")
-        result = manager._get_column_names_and_types_from_df(df)
-        expected_result = {
-            "Column string": type_analyzer.Type.STRING,
-            "Column string all lines with value": type_analyzer.Type.STRING,
-            "Column integer": type_analyzer.Type.INTEGER,
-            "Column decimal": type_analyzer.Type.DECIMAL,
-        }
+        result = [result_yield for result_yield in manager._get_column_names_and_types_from_df(df)]
+        expected_result = [
+            ("Column string", type_analyzer.Type.STRING),
+            ("Column string all lines with value", type_analyzer.Type.STRING),
+            ("Column integer", type_analyzer.Type.INTEGER),
+            ("Column decimal", type_analyzer.Type.DECIMAL),
+        ]
         self.assertEqual(result, expected_result)
 
 
