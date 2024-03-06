cdee039eb05589347b6c1b6ed9e1379930aa92cc
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-03-06 22:42:06 +0100

    Improve read cr file

--- src/extractors.py
+++ src/extractors.py
@@ -11,11 +11,15 @@ def get_df_from_csv(path_name: str) -> Df:
 
 
 def _get_df_from_cr_csv(path_name) -> Df:
+    from src import constants
+
     return pd.read_csv(
         path_name,
         dtype=str,
         sep="|",
+        names=constants.COLUMN_NAMES,
         quoting=csv.QUOTE_NONE,
         skipinitialspace=True,
         na_values=["N.A."],
+        usecols=constants.COLUMN_NAMES_TO_USE,
     )
